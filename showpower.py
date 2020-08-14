b72b0b85da3c095d1d6bb735a1c2cab9698edd92
debiangamer <fin4478@hotmail.com>
2020-08-14 16:49:17 +0300

    Supports PS4 only

--- showpower.py
+++ showpower.py
@@ -12,10 +12,10 @@ class MyWindow(Gtk.Window):
 		self.add(self.box)
 		try:
 			a = subprocess.check_output(['cat', '/sys/class/power_supply/sony_controller_battery_98:b6:e9:49:7d:06/capacity'])
+			power_label = Gtk.Label(label="Power Level Percentage = " + str(int(a)))
 		except:
-			a="Please connect the  controller"
-		label = Gtk.Label(label="Power Level Percentage = " + str(int(a)))
-		self.box.pack_start(label, True, True, 0)
+			power_label = Gtk.Label(label="Please connect the  controller")
+		self.box.pack_start(power_label, True, True, 0)
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
